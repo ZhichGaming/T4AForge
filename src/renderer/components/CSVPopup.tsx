78057faafd9d3fc9d55a4960635591952d548223
ZhichGaming <nick.zhicheng@gmail.com>
2025-03-29 14:52:08 -0400

    feat: csv import review page

--- src/renderer/components/CSVPopup.tsx
+++ src/renderer/components/CSVPopup.tsx
@@ -4,12 +4,18 @@ import "./CSVPopup.scss"
 import findFirstMatch from "../utils/findFirstMatch";
 import { T4A_DYNAMIC_FIELD_MAPPINGS, T4A_FIELD_MAPPINGS } from "../utils/FIELD_MAPPINGS";
 import { DYNAMIC_FIELD_TITLES, FIELD_TITLES } from "../utils/FIELD_TITLES";
+import { T4ASlipData, T4ASlipDynamicKeys, T4ASlipKeys } from "../types/T4A.types";
 import access from "../utils/access";
-import { T4ASlipDynamicKeys, T4ASlipKeys } from "../types/T4A.types";
 
-async function extractFieldsFromCSV(csvPath: string): Promise<{ [key: string]: (keyof T4ASlipKeys) | (keyof T4ASlipDynamicKeys)[] }> {
-  const [columns, ...csv] = await window.manageCSV.getCSV(csvPath) as string[][]
-  const keys: { [key: string]: (string | string[]) } = {}
+async function getCSV(csvPath: string) {
+  const csv = await window.manageCSV.getCSV(csvPath)
+
+  return csv as string[][]
+}
+
+async function extractColumnsFromCSV(csv: string[][]): Promise<{ [key: string]: string }> {
+  const columns = Array.from(csv[0])
+  const keys: { [key: string]: string } = {}
 
   function assignKey<
     T extends keyof typeof T4A_FIELD_MAPPINGS, // Top-level key
@@ -44,7 +50,7 @@ async function extractFieldsFromCSV(csvPath: string): Promise<{ [key: string]: (
 
   const dynamicNameMatch = findFirstMatch(columns, T4A_DYNAMIC_FIELD_MAPPINGS.recipientName)
   if (dynamicNameMatch) {
-    keys[dynamicNameMatch] = ["recipientName", "recipientCorpName"]
+    keys[dynamicNameMatch] = "recipientName"
     columns.splice(columns.indexOf(dynamicNameMatch), 1)
   }
 
@@ -59,7 +65,7 @@ async function extractFieldsFromCSV(csvPath: string): Promise<{ [key: string]: (
 
   const dynamicAddressMatch = findFirstMatch(columns, T4A_DYNAMIC_FIELD_MAPPINGS.recipientAddress)
   if (dynamicAddressMatch) {
-    keys[dynamicAddressMatch] = ["recipientAddress"]
+    keys[dynamicAddressMatch] = "recipientAddress"
     columns.splice(columns.indexOf(dynamicAddressMatch), 1)
   }
 
@@ -83,74 +89,202 @@ async function extractFieldsFromCSV(csvPath: string): Promise<{ [key: string]: (
     keys[column] = ""
   })
 
-  return keys as { [key: string]: (keyof T4ASlipKeys) | (keyof T4ASlipDynamicKeys)[] }
+  return keys as { [key: string]: string }
+}
+
+function getFieldList(titles: string[], mapping: { [key: string]: string }) {
+  const fields: string[] = []
+
+  for (const title of titles) {
+    const key = mapping[title]
+
+    if (key.length > 0) {
+      fields.push(key)
+    }
+  }
+
+  return fields
+}
+
+function buildT4aSlips(csv: string[][], mappingKeys: { [key: string]: string }, fieldList: string[]) {
+  const slips: T4ASlipData[] = []
+
+  for (let i = 1; i < csv.length; i++) {
+    const row = csv[i]
+    const slip = new T4ASlipData()
+
+    for (let j = 0; j < row.length; j++) {
+      const columnKey = mappingKeys[csv[0][j]]
+      const value = row[j]
+
+      if (!fieldList.includes(columnKey)) continue
+      if (value.length === 0) continue
+
+      const splitKey = columnKey.split(".")
+
+      if (splitKey.length === 1) {
+        if (slip.hasOwnProperty(columnKey)) {
+          (slip[columnKey as keyof T4ASlipData] as any) = value
+        }
+      } else {
+        const parentKey = splitKey[0] as keyof T4ASlipData
+        const childKey = splitKey[1] as keyof T4ASlipData[typeof parentKey]
+
+        if (slip[parentKey]) {
+          (slip[parentKey][childKey] as any) = value
+        }
+      }
+    }
+
+    slips.push(slip)
+  }
+
+  return slips
 }
 
 export default function CSVPopup() {
   const [isOpen, setIsOpen] = useState(false);
-  const [page, setPage] = useState<'config' | 'confirm'>("config")
-  const [csvPath, setCsvPath] = useState('')
-  const [mappingKeys, setMappingKeys] = useState<{ [key: string]: (keyof T4ASlipKeys) | (keyof T4ASlipDynamicKeys)[] }>({})
+  const [page, setPage] = useState<'config' | 'loading' | 'confirm'>("config")
+  const [csv, setCSV] = useState<string[][]>();
+  const [mappingKeys, setMappingKeys] = useState<{ [key: string]: string }>({})
 
-  const onFileImport = async (csvPath: string) => {
-    const keys = await extractFieldsFromCSV(csvPath)
+  const [fieldList, setFieldList] = useState<string[]>([])
+  const [slips, setSlips] = useState<T4ASlipData[]>([])
+
+  const onFileImport = async (csv: string[][]) => {
+    const keys = await extractColumnsFromCSV(csv)
     setMappingKeys(keys)
   }
 
+  const nextPage = () => {
+    if (page == "loading") {
+      setPage("confirm")
+    }
+
+    if (page == "config" && findFirstMatch(Object.values(mappingKeys), Object.keys(DYNAMIC_FIELD_TITLES))) {
+      setPage("loading")
+    } else if (page == "config") {
+      setPage("confirm")
+    }
+  }
+
+  const closeModal = () => {
+    setIsOpen(false)
+    setPage("config")
+    setCSV(undefined)
+    setMappingKeys({})
+  }
+
+  useEffect(() => {
+    const newFieldList = getFieldList(Object.keys(mappingKeys), mappingKeys)
+    setFieldList(newFieldList)
+
+    setSlips(buildT4aSlips(csv || [], mappingKeys, newFieldList))
+  }, [csv, mappingKeys])
+
   return (
-    <Popup
-      open={isOpen}
-      onClose={() => setIsOpen(false)}
-      trigger={<button type="button" className='pre-form-button'>Import CSV</button>}
-      modal
-      nested
-    >
-      <div className="popup-content-container">
-        <h2>Import CSV</h2>
-        <hr />
-        <input
-          id='csv-input'
-          type="file"
-          onChange={(e) => {
-            const path = (document.getElementById('csv-input') as HTMLInputElement).files?.[0]?.path
+    <>
+      <button type="button" className='pre-form-button' onClick={() => setIsOpen(true)}>Import CSV</button>
+      <Popup
+        open={isOpen}
+        onClose={closeModal}
+        modal
+        nested
+      >
+        <div className="popup-content-container">
+          <h2>Import CSV</h2>
+          <hr />
+          {page == "config" ?
+            <div className="config-container">
+              <input
+              id='csv-input'
+              type="file"
+              onChange={(e) => {
+                const path = (document.getElementById('csv-input') as HTMLInputElement).files?.[0]?.path
 
-            if (!path) return
+                if (!path) return
 
-            setCsvPath(path)
-            onFileImport(path)
-          }}
-          className='file-input'
-          accept=".csv" />
-        <p>CSV Column Titles</p>
-        <ul className='list'>
-          {Object.entries(mappingKeys).map(([column, key]) => (
-            <li key={column}>
-              <span>{column}</span>
-              <select className="key-selector" value={key}>
-                {Object.entries(FIELD_TITLES).map(([key, value]) => {
-                  if (value instanceof Object) {
-                    return Object.entries(value).map(([sKey, sValue]) => <option value={key + "." + sKey}>{String(sValue)}</option>)
+                getCSV(path).then((csv) => {
+                  setCSV(csv)
+                  onFileImport(csv)
+                })
+              }}
+              className='file-input'
+              accept=".csv" />
+            <p>CSV Column Titles</p>
+            <ul className='list'>
+              {Object.entries(mappingKeys).map(([column, key]) => (
+                <li key={column}>
+                  <span>{column}</span>
+                  <select
+                    className="key-selector"
+                    value={key}
+                    onChange={(e) => {
+                      const value = e.target.value
+                      setMappingKeys({ ...mappingKeys, [column]: value as (keyof T4ASlipKeys) | (keyof T4ASlipDynamicKeys) })
+                    }}>
+                    {Object.entries(FIELD_TITLES).map(([key, value]) => {
+                      if (value instanceof Object) {
+                        return Object.entries(value).map(([sKey, sValue]) => <option value={key + "." + sKey} key={key + "." + sKey}>{String(sValue)}</option>)
+                      }
+
+                      return <option value={key} key={key}>{value}</option>
+                    })}
+                    {Object.entries(DYNAMIC_FIELD_TITLES).map(([key, value]) => {
+                      if (value instanceof Object) {
+                        return Object.entries(value).map(([sKey, sValue]) => <option value={key + "." + sKey} key={key + "." + sKey}>Dynamic - {String(sValue)}</option>)
+                      }
+
+                      return <option value={key} key={key}>Dynamic - {value}</option>
+                    })}
+                    <option value=''>No match</option>
+                  </select>
+                </li>
+              ))}
+            </ul>
+            <hr />
+            <div className="actions">
+              <button type="button" className='cancel' onClick={closeModal}>Cancel</button>
+              <button type="button" className='next' onClick={nextPage}>Next</button>
+            </div>
+          </div> :
+          <>
+            <div className="confirm-container">
+              <table>
+                <thead>
+                  <tr>
+                    {fieldList.map((cell, i) => (
+                      <th key={i}>{
+                        // TODO: This should be an exclusively normal field title when I'm done with the dynamic fields
+                        access(cell, FIELD_TITLES) instanceof Object ?
+                          access(cell, DYNAMIC_FIELD_TITLES) :
+                          access(cell, FIELD_TITLES)
+                      }</th>
+                    ))}
+                  </tr>
+                </thead>
+                <tbody>
+                  {
+                    slips.map((slip, i) => (
+                      <tr key={i}>
+                        {fieldList.map((cell, i) => (
+                          <td key={i}>{access(cell, slip)}</td>
+                        ))}
+                      </tr>
+                    ))
                   }
-
-                  return <option value={key}>{value}</option>
-                })}
-                {Object.entries(DYNAMIC_FIELD_TITLES).map(([key, value]) => {
-                  if (value instanceof Object) {
-                    return Object.entries(value).map(([sKey, sValue]) => <option value={key + "." + sKey}>Dynamic - {String(sValue)}</option>)
-                  }
-
-                  return <option value={key}>Dynamic - {value}</option>
-                })}
-                <option value=''>No match</option>
-              </select>
-            </li>
-          ))}
-        </ul>
-        <hr />
-        <div className="actions">
-          <button type="button" className='next' onClick={() => setPage("confirm")}>Next</button>
+                </tbody>
+              </table>
+            </div>
+            <hr />
+            <div className="actions">
+              <button type="button" className='cancel' onClick={closeModal}>Cancel</button>
+              {/* <button type="button" className='next' onClick={nextPage}>Next</button> */}
+            </div>
+          </>
+          }
         </div>
-      </div>
-    </Popup>
+      </Popup>
+    </>
   )
 }
