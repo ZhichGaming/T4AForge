ac205880ef752fd928cb996d2eb6b8ded5e9a1a9
ZhichGaming <nick.zhicheng@gmail.com>
2025-04-03 00:16:18 -0400

    chore: version and copyright

--- src/renderer/preload.d.ts
+++ src/renderer/preload.d.ts
@@ -1,8 +1,9 @@
-import { ManageCSV, ManagePresets, ManageSubmissions } from '../main/preload';
+import { GetVersion, ManageCSV, ManagePresets, ManageSubmissions } from '../main/preload';
 
 declare global {
   // eslint-disable-next-line no-unused-vars
   interface Window {
+    getVersion: GetVersion;
     managePresets: ManagePresets;
     manageSubmissions: ManageSubmissions;
     manageCSV: ManageCSV;
